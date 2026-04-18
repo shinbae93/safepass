c59ca64a5b5166278d2dc1919149b4129922c887
Hung Nguyen <sine.hungnguyen@gmail.com>
2026-04-18 20:49:27 +0700

    chore: add root ESLint base config, extend from apps/api

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -1,7 +1,5 @@
-const tseslint = require('@typescript-eslint/eslint-plugin');
 const tsparser = require('@typescript-eslint/parser');
-const prettier = require('eslint-plugin-prettier');
-const prettierConfig = require('eslint-config-prettier');
+const base = require('../../eslint.config.base');
 
 module.exports = [
   {
@@ -18,19 +16,7 @@ module.exports = [
         jest: true,
       },
     },
-    plugins: {
-      '@typescript-eslint': tseslint,
-      prettier,
-    },
-    rules: {
-      ...tseslint.configs['recommended'].rules,
-      ...prettierConfig.rules,
-      'prettier/prettier': 'error',
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    },
+    plugins: base.plugins,
+    rules: base.rules,
   },
 ];
